886b24c99fe774a00d65183e8668a5168f1d8c48
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-07-04 16:51:50 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,6 +2,10 @@ class Backer
   attr_reader :backed_projects
   def initialize
     @backed_projects = []
+  end
 
+  def back_project()
+    project = Project.new
+    @backed_projects << project
   end
 end
